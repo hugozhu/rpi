e28298a728ec78204752932487970da2bc9c2367
hugozhu <hugozhu@gmail.com>
2013-04-06 21:12:41 +0800

    added pcd8544 lib

---
+++ lib/PCD8544/build.sh
@@ -0,0 +1,1 @@
+gcc PCD8544.c -shared -o libPCD8544.so
--- pcd8544/pcd8544.go
+++ pcd8544/pcd8544.go
@@ -1,12 +1,20 @@
 package pcd8544
 
 /*
-#cgo LDFLAGS: -lwiringPi -lPCD8544
+#cgo LDFLAGS: -L . -lPCD8544 -lwiringPi
 
 #include <wiringPi.h>
 #include <PCD8544.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+
+void Test(char *c, char *c2) {
+	LCDclear();
+	LCDdrawstring(0, 24, c);
+	LCDdrawstring(0, 34, c2);
+	LCDdisplay();
+}
 */
 import "C"
 
@@ -19,11 +27,15 @@ func LCDclear() {
 	C.LCDclear()
 }
 
+func Test(s1 string, s2 string) {
+	C.Test(C.CString(s1), C.CString(s2))
+}
+
 func LCDInit(_sclk int, _din int, _dc int, _cs int, _rst int, contrast int) {
 	C.LCDInit(C.uint8_t(_sclk), C.uint8_t(_din), C.uint8_t(_dc), C.uint8_t(_cs), C.uint8_t(_rst), C.uint8_t(contrast))
 }
 
-func LCDcommand(c int8) {
+func LCDcommand(c int) {
 	C.LCDcommand(C.uint8_t(c))
 }
 
